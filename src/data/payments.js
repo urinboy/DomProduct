cfff98872432efd03aac349f3264dfcb0c5ea7bf
Urinboy <urinboytursunboev@gmail.com>
2025-07-26 06:53:50 +0500

    yangi next.js o'rnatildi

--- src/data/payments.js
+++
@@ -1,6 +0,0 @@
-export const payments = [
-  { id: 1, name: 'Naqd pul', icon: 'fas fa-money-bill-wave' },
-  { id: 2, name: 'Karta orqali', icon: 'fas fa-credit-card' },
-  { id: 3, name: 'Payme', icon: 'fas fa-wallet' },
-  { id: 4, name: 'Click', icon: 'fas fa-wallet' },
-];
